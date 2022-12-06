accda116071916e617df5188db28353dc04a677b
stainsleto <stainsmault@gmail.com>
2022-12-06 21:22:27 +0530

    Search component added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Hero from './components/hero'
 import './App.css';
+import SearchBox from './components/searchBox'
 
 function App() {
   return (
-    <Hero />
+    <div>
+      <Hero />
+      <SearchBox />
+    </div>
+    
   );
 }
 

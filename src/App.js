63ae8fcbf578c96ac877c4ddd43776467a400279
stainsleto <stainsmault@gmail.com>
2022-12-06 22:59:05 +0530

    Advanced component added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Hero from './components/hero'
 import './App.css';
 import SearchBox from './components/searchBox'
+import Advanced from './components/advanced'
 
 function App() {
   return (
     <div>
       <Hero />
       <SearchBox />
+      <Advanced />
+      
     </div>
     
   );

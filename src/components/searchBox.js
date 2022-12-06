5a1a14061a3dae9b5ee22db616190ffc52eaa24c
stainsleto <stainsmault@gmail.com>
2022-12-06 21:20:26 +0530

    Search Box

---
+++ src/components/searchBox.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../App.css'
+
+function SearchBox(){
+    return(
+        <div id="search-box-main">
+            <div>
+                <input type="text" />
+                <button>Shorten it !</button>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBox;

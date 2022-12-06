b67cf8599367c828bd9727d1c889372a6382d03a
Samnathsha <samnath.502039@sxcce.edu.in>
2022-12-06 19:29:16 +0530

    branch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from './components/hero'
+import Banner from './components/banner';
 import './App.css';
+import './sam.css';
+
 
 function App() {
   return (
     <Hero />
+    <Banner />
   );
 }
 
---
+++ src/components/banner.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../sam.css'
+
+function Banner (){
+    return(
+
+  <div>
+    <H1>
+        HII
+    </H1>
+  </div>
+
+
+
+
+
+
+
+
+
+    )}
+export default Banner;

af84cbfdae5c4f3e61a1567c38da29abf08cba9a
stainsleto <stainsmault@gmail.com>
2022-12-06 22:57:43 +0530

    other than nav hero done

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import '../App.css'
 import logo from '../images/logo.svg'
 import heroImg from '../images/illustration-working.svg';
 

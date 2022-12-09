cc3a556f84cc2c559d1643de1c3fa68c25127341
stainsleto <stainsmault@gmail.com>
2022-12-09 23:54:20 +0530

    Function added for Url shorten

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import logo from '../images/logo.svg'
 import heroImg from '../images/illustration-working.svg';
-import { click } from '@testing-library/user-event/dist/click';
+
+
 
 function Hero() {
 
-    const searchButton = document.getElementById("search-button");
-    searchButton.addEventListener("click",() => {
+    function urlShort() {
     const searchBar = document.getElementById("search-bar").value;
     const api = fetch(`https://api.shrtco.de/v2/shorten?url=${searchBar}`);
-    const link = api.then( (response) => response.json());
-    console.log(link);
-    const result = document.getElementById("result");
-    result.textContent = link.result.full_short_link ;
+    const link = api.then( (response) => response.json())
+    .then( (data)=> {
+        const result = document.getElementById("result");
+        result.textContent = data.result.full_short_link ;
 
-    });
+    })
+    console.log(link);
+
+    
+
+    };
 
 
     return (
@@ -30,8 +35,8 @@ function Hero() {
                 </div>
 
                 <div id="right-nav">
-                    <button>Login</button>
-                    <button>Sign Up</button>
+                    <button className="hero-button">Login</button>
+                    <button className="hero-button">Sign Up</button>
                 </div>
 
             </div>
@@ -40,7 +45,7 @@ function Hero() {
                 <div id="hero-left">
                     <h2 id="hero-title">More than just <br /> shorter links</h2><br />
                     <p id="hero-description">Buld your brand's recognition and get <br />detailed insights on how your links are performing</p> <br />
-                    <button id="hero-button">Get Started</button>
+                    <button className="hero-button">Get Started</button>
                 </div>
                 <div id="hero-right">
                     <img src={heroImg} />
@@ -48,11 +53,13 @@ function Hero() {
             </div>
 
             <div id="search-box-layout">
+                
                 <div id="search-box">
                     <input id="search-bar" type="text" placeholder="Shorten a link here.." />
-                    <button id="search-button">Shorten it !</button>
+                    <button onClick={urlShort} id="search-button"> Shorten it!</button>
                     
                 </div>
+                
                 
             </div>
             <h1 id="result">result</h1>
